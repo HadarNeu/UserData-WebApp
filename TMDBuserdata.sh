0be659886b1997f3fdd1adaf57d50bb437296a0c
Hadar Neulander <hadarnoylander@gmail.com>
2022-11-14 15:30:12 +0200

    small changes

--- TMDBuserdata.sh
+++ TMDBuserdata.sh
@@ -5,13 +5,13 @@ usermod -a -G docker ec2-user
 pip3 install docker-compose
 systemctl enable docker
 systemctl start docker
-yum-config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+yum-config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo #in order to be able to install rpm packages
 yum install gh -y
 cd /home/ec2-user/
-git clone https://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:x-oauth-basic@github.com/Yevgeniis/aws_restart_course.git
-echo "API_Key='<insert here your API key>'" >> ./aws_restart_course/.env
-chown -R ec2-user:ec2-user /home/ec2-user/aws_restart_course
-cd ./aws_restart_course/
+git clone https://github.com/HadarNeu/TMDB-Group-Project.git
+echo "API_Key='<insert here your API key>'" >> ./TMDB-Group-Project/.env
+chown -R ec2-user:ec2-user /home/ec2-user/TMDB-Group-Project #i want my user to have access to the directory!
+cd ./TMDB-Group-Project/
 docker-compose up -d
 
 
